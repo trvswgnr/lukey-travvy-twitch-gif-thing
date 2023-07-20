97d0bda9bb8374b9c68af266d659bef8ad20033c
Travis Wagner <travis.wagner@umusic.com>
2023-07-20 19:08:56 -0400

    bunch of garbage

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+use reqwest::{get, Body, Client, Url};
+use serde::{Deserialize, Serialize};
+use serde_json::from_str;
+use serde_json::Value;
 use std::fmt;
 use tokio::fs;
 
@@ -17,12 +21,13 @@ impl fmt::Display for Status {
     }
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 enum Route {
     Index,
     Hello,
     Send,
     NotFound,
+    Search,
 }
 
 impl fmt::Display for Route {
@@ -32,6 +37,7 @@ impl fmt::Display for Route {
             Route::Hello => "hello",
             Route::Send => "send",
             Route::NotFound => "404",
+            Route::Search => "search",
         };
 
         write!(f, "{}", name)
@@ -59,6 +65,7 @@ impl From<&str> for Route {
             "/" => Route::Index,
             "/hello" => Route::Hello,
             "/send" => Route::Send,
+            "/search" => Route::Search,
             _ => Route::NotFound,
         }
     }
@@ -67,6 +74,7 @@ impl From<&str> for Route {
 pub struct Request {
     method: Method,
     route: Route,
+    // body: Option<Body>,
 }
 
 impl From<&str> for Request {
@@ -80,8 +88,32 @@ impl From<&str> for Request {
         let method = Method::from(parts.next().unwrap());
         let route = Route::from(parts.next().unwrap());
 
-        Self { method, route }
+        Self {
+            method,
+            route,
+            // body: Some(Body::from(body)),
+        }
     }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct GiphyResponse {
+    data: Vec<Gif>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Gif {
+    images: Images,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Images {
+    downsized: Downsized,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Downsized {
+    url: String,
 }
 
 pub struct Response {
@@ -98,18 +130,66 @@ impl Response {
                 // we could rate limit or something here
                 (Status::Ok, request.route)
             }
+            (Method::Post, Route::Search) => (Status::Ok, request.route),
+            (Method::Get, Route::Search) => (Status::Ok, request.route),
             _ => (Status::NotFound, Route::NotFound),
         };
 
         Self { status, route }
     }
 
-    pub async fn to_string(&self) -> String {
+    pub async fn to_string(&self) -> Result<String, Box<dyn std::error::Error>> {
+        if self.route == Route::Search {
+            let client = Client::new();
+            let api_key: String = get_env_var("GIPHY_KEY").unwrap();
+            let endpoint = format!(
+                "https://api.giphy.com/v1/gifs/search?api_key={api_key}&q=pog&limit=25&offset=0"
+            );
+
+            let giphy_response = reqwest::get(&endpoint)
+                .await?
+                .json::<GiphyResponse>()
+                .await?;
+
+            let gif_objs = giphy_response.data;
+
+            for gif in gif_objs {
+                let gif_url = gif.images.downsized.url;
+
+                println!("URL: {}", gif_url);
+            }
+
+            return Ok(format!("{}{}", self.status, "body"));
+        }
         let path = format!("routes/{}.html", self.route);
         let body = fs::read_to_string(path)
             .await
             .unwrap_or_else(|_| String::from("Error reading file"));
 
-        format!("{}{}", self.status, body)
+        Ok(format!("{}{}", self.status, body))
+    }
+}
+
+use dotenv::dotenv;
+use std::{env, error::Error, str::FromStr, sync::Once};
+
+static INIT: Once = Once::new();
+
+pub fn get_env_var<T: FromStr>(key: &str) -> Result<T, Box<dyn Error>> {
+    INIT.call_once(|| match dotenv().ok() {
+        Some(_) => println!(".env file detected, loading..."),
+        None => println!("No .env file found."),
+    });
+
+    match env::var(key) {
+        Ok(val) => {
+            let parsed: Result<T, Box<dyn Error>> = match val.parse::<T>() {
+                Ok(parsed) => Ok(parsed),
+                Err(_) => Err(format!("Failed to parse {key}").into()),
+            };
+
+            parsed
+        }
+        Err(_) => Err(format!("{key} not set").into()),
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,31 @@ use tokio::{
     net::TcpListener,
 };
 
-use lib::{Request, Response};
+use serde::{Deserialize, Serialize};
+
+use lib::{get_env_var, Request, Response};
 
 const BUFFER_SIZE: usize = 1024;
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct GiphyResponse {
+    data: Vec<Gif>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Gif {
+    images: Images,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Images {
+    downsized: Downsized,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Downsized {
+    url: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -21,11 +43,67 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                             return;
                         }
 
-                        let request_str = String::from_utf8_lossy(&buf[..n]);
-                        let request = Request::from(request_str.as_ref());
-                        let response = Response::from(request).to_string().await;
+                        let mut response_string = "".to_string();
 
-                        if let Err(e) = stream.write_all(response.as_bytes()).await {
+                        let request_string = String::from_utf8_lossy(&buf[..n]);
+                        let mut lines = request_string.lines();
+
+                        let request_line = lines.next().unwrap();
+
+                        if request_line == "GET / HTTP/1.1" {
+                            let content = include_str!("../routes/index.html");
+                            response_string = format!(
+                                "HTTP/1.1 {}\r\nContent-Type: text/html\r\n\r\n{}",
+                                200, content
+                            );
+                        }
+
+                        if request_line == "POST /search HTTP/1.1" {
+                            for line in lines.by_ref() {
+                                if line.trim().is_empty() {
+                                    break;
+                                }
+                            }
+
+                            let body: String = lines.collect();
+
+                            let search_query = body.split('=').last().unwrap();
+
+                            let api_key: String = get_env_var("GIPHY_KEY").unwrap();
+
+                            let endpoint = format!("https://api.giphy.com/v1/gifs/search?api_key={api_key}&q={search_query}&limit=25&offset=0");
+
+                            let giphy_response = reqwest::get(endpoint)
+                                .await
+                                .unwrap()
+                                .json::<GiphyResponse>()
+                                .await
+                                .unwrap();
+                            let gif_objs = giphy_response.data;
+
+                            let mut gif_cards = String::new();
+
+                            for gif in gif_objs {
+                                let gif_url = gif.images.downsized.url;
+
+                                gif_cards.push_str(&giphy_card(&gif_url));
+
+                                println!("URL: {}", gif_url);
+                            }
+
+                            response_string = format!(
+                                "HTTP/1.1 {}\r\nContent-Type: text/html\r\n\r\n{}",
+                                200, gif_cards
+                            );
+                            println!("search query = {search_query}");
+                        }
+
+                        // println!("{request_string}");
+
+                        // let lines = request_string.line();
+                        // let req_header = lines.next().unwrap();
+
+                        if let Err(e) = stream.write_all(response_string.as_bytes()).await {
                             eprintln!("failed to write to socket; err = {:?}", e);
                             return;
                         }
@@ -45,4 +123,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             }
         });
     }
+}
+
+const GIPHY_CARD_PARTIAL: &str = include_str!("../partials/giphy-card.html");
+fn giphy_card(src: &str) -> String {
+    GIPHY_CARD_PARTIAL.replace("{src}", src)
 }
